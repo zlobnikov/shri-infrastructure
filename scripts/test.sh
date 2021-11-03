a4216e7f8df33bdd0e4d8939935c1b8834ad6e5f
zlobnikov <daffodil@list.ru>
2021-11-03 17:18:39 +0300

    feat: get data from post

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,16 +8,24 @@ SEARCH_URL="https://api.tracker.yandex.net/v2/issues/_search"
 VERSION=$(git tag --sort version:refname | tail -1 | head -1)
 UNIQUE_KEY="zlobnikov, ${VERSION}"
 
-TICKET=$(
+TICKET_URL=$(
   curl -s -X POST ${SEARCH_URL} \
   --header "Authorization: OAuth ${OAuth}" \
   --header "X-Org-ID: ${OrgId}" \
   --header 'Content-Type: application/json' \
-  --data "{\"filter\": {\"unique\": \"$UNIQUE_KEY\"} }"
+  --data "{\"filter\": {\"unique\": \"$UNIQUE_KEY\"} }" | jq -r '.[].self'
 )
 
-TICKET_URL=$(${TICKET} | jq ".[].self")
-TICKET_DESC=$(${TICKET} | jq ".[].description")
+TICKET_DESC=$(
+  curl -s -X POST ${SEARCH_URL} \
+  --header "Authorization: OAuth ${OAuth}" \
+  --header "X-Org-ID: ${OrgId}" \
+  --header 'Content-Type: application/json' \
+  --data "{\"filter\": {\"unique\": \"$UNIQUE_KEY\"} }" | jq -r '.[].description'
+)
+
+# TICKET_URL=$(jq ".[].self" ${TICKET})
+# TICKET_DESC=$(${TICKET} | jq '.[].description')
 
 echo "URL: ${TICKET_URL}"
 echo "Desc: ${TICKET_DESC}"
