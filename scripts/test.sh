a485bbc6dea5d158232643e25169cf50b90068a2
zlobnikov <daffodil@list.ru>
2021-11-03 15:45:46 +0300

    debug: look for bad substitution

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,16 +3,22 @@
 RESULT=$(npm test 2>&1)
 # echo "Tests Results:\n${RESULT}"
 
+echo "getting release version"
+
 SEARCH_URL="https://api.tracker.yandex.net/v2/issues/_search"
 
 VERSION=$(git tag --sort version:refname | tail -1 | head -1)
 UNIQUE_KEY="zlobnikov, ${VERSION}"
+
+echo "forming search request"
 
 SEARCH_REQUEST=${
   "filter": {
     "unique": "'"${UNIQUE_KEY}"'"
   }
 }
+echo "search request:"
+# echo ${SEARCH_REQUEST}
 
 TICKET=$(
   curl -s -X POST ${SEARCH_URL} \
