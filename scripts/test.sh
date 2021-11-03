1e95fc54aa5df93f51a160e6362563d64b19efe9
zlobnikov <daffodil@list.ru>
2021-11-03 15:58:49 +0300

    fix: delete extra bracket

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,30 +1,18 @@
 #!/usr/bin/env bash
 
 RESULT=$(npm test 2>&1)
-echo "Tests Results:\n${RESULT}"
-
-echo "getting release version"
+echo "Tests Results:\n${RESULT}\n"
 
 SEARCH_URL="https://api.tracker.yandex.net/v2/issues/_search"
 
 VERSION=$(git tag --sort version:refname | tail -1 | head -1)
 UNIQUE_KEY="zlobnikov, ${VERSION}"
 
-echo "forming search request"
-
-# SEARCH_REQUEST=${
-#   "filter": {
-#     "unique":"'"${UNIQUE_KEY}"'"
-#   }
-# }
-# echo "search request:"
-# echo ${SEARCH_REQUEST}
-
 TICKET=$(
   curl -s -X POST ${SEARCH_URL} \
   --header "Authorization: OAuth ${OAuth}" \
   --header "X-Org-ID: ${OrgId}" \
   --header 'Content-Type: application/json' \
-  --data "{\"filter\": {\"unique\": \"$UNIQUE_KEY\"} }")
+  --data "{\"filter\": {\"unique\": \"$UNIQUE_KEY\"} }"
 )
 echo "Ticket: ${TICKET}"
