7269bca185b5145dcf39fbb69a9709d13bcebb14
zlobnikov <daffodil@list.ru>
2021-11-03 14:00:21 +0300

    feat: add tests

---
+++ test.js
@@ -0,0 +1,9 @@
+describe('Simple Test Case', () => {
+    it('Should return 4', () => {
+        expect(2 + 2).toBe(4);
+    });
+
+    it('Should return 60', () => {
+        expect(20 * 3).toBe(60);
+    });
+});
