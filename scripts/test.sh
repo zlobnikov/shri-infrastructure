7ad007ac702ba7a3a68335a326e94133788e31a0
zlobnikov <daffodil@list.ru>
2021-11-03 21:53:39 +0300

    debug: isolate patching

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,40 +1,45 @@
 #!/usr/bin/env bash
 
-RESULT=$(npm test 2>&1)
-echo "\nTests Results:\n${RESULT}\n"
+# RESULT=$(npm test 2>&1)
+# echo "\nTests Results:\n${RESULT}\n"
 
-SEARCH_URL="https://api.tracker.yandex.net/v2/issues/_search"
+# SEARCH_URL="https://api.tracker.yandex.net/v2/issues/_search"
 
-VERSION=$(git tag --sort version:refname | tail -1 | head -1)
-UNIQUE_KEY="zlobnikov, ${VERSION}"
+# VERSION=$(git tag --sort version:refname | tail -1 | head -1)
+# UNIQUE_KEY="zlobnikov, ${VERSION}"
 
-TICKET_URL=$(
-  curl -s -X POST ${SEARCH_URL} \
-  --header "Authorization: OAuth ${OAuth}" \
-  --header "X-Org-ID: ${OrgId}" \
-  --header 'Content-Type: application/json' \
-  --data "{\"filter\": {\"unique\": \"$UNIQUE_KEY\"} }" | jq -r '.[].self'
-)
+# TICKET_URL=$(
+#   curl -s -X POST ${SEARCH_URL} \
+#   --header "Authorization: OAuth ${OAuth}" \
+#   --header "X-Org-ID: ${OrgId}" \
+#   --header 'Content-Type: application/json' \
+#   --data "{\"filter\": {\"unique\": \"$UNIQUE_KEY\"} }" | jq -r '.[].self'
+# )
 
-TICKET_DESC=$(
-  curl -s -X POST ${SEARCH_URL} \
-  --header "Authorization: OAuth ${OAuth}" \
-  --header "X-Org-ID: ${OrgId}" \
-  --header 'Content-Type: application/json' \
-  --data "{\"filter\": {\"unique\": \"$UNIQUE_KEY\"} }" | jq -r '.[].description'
-)
+# TICKET_DESC=$(
+#   curl -s -X POST ${SEARCH_URL} \
+#   --header "Authorization: OAuth ${OAuth}" \
+#   --header "X-Org-ID: ${OrgId}" \
+#   --header 'Content-Type: application/json' \
+#   --data "{\"filter\": {\"unique\": \"$UNIQUE_KEY\"} }" | jq -r '.[].description'
+# )
 
-echo "Ticket URL: ${TICKET_URL}"
-echo "Desc:\n${TICKET_DESC}"
+# echo "Ticket URL: ${TICKET_URL}"
+# echo "Desc:\n${TICKET_DESC}"
 
-UPDATED_DESC=" ${TICKET_DESC}\nTests Results:\n${RESULT}"
+# UPDATED_DESC=" ${TICKET_DESC}\nTests Results:\n${RESULT}"
+
+TICKET_URL="https://api.tracker.yandex.net/v2/issues/TMP-1025"
+UPDATED_DESC="patch #1"
+
+echo 'start'
 
 RESPONSE=$(
   curl -s -X PATCH ${TICKET_URL} \
   --header "Authorization: OAuth ${OAuth}" \
   --header "X-Org-ID: ${OrgId}" \
   --header "Content-Type: application/json" \
-  --data "{\"description\": \"$UPDATED_DESC\"}"
+  --data "{\"description\": \"${UPDATED_DESC}\"}"
 )
 echo "Response: ${RESPONSE}."
 
